d6c1edd8334b9b50d6837bc37ced1f5717154494
Angeline Wang <02angelinewang@gmail.com>
2022-08-30 21:14:12 +0200

    Set up React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {Link} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+
+    return (
+        <>
+        <Nav/> 
+        </>
+    )
+}
+
+export default Header;
