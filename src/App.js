3a7e57ffb81654e5f227923d6fc9d4bf6854828a
Angeline Wang <02angelinewang@gmail.com>
2022-09-01 21:15:19 +0100

    Sort bar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 import {Link} from 'react-router-dom'
+import React, { useEffect } from 'react';
 
 function App() {
+
+  const [Books, setBooks] = React.useState([]);
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch();
+      const bookData
+      
+    }
+  })
+
   return (
     <div className="App">
      
