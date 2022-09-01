29974a74419cbbf401c2a5b8182362048d89e1e3
Angeline Wang <02angelinewang@gmail.com>
2022-09-01 21:38:15 +0100

    Adding extra fetch data

--- src/App.js
+++ src/App.js
@@ -10,7 +10,13 @@ function App() {
         "http://openlibrary.org/search.json?author=tolkien"
       );
       const bookData = await response.json();
-      setBooks(bookData);
+      const response2 = await fetch("http://openlibrary.org/search.json?author=leo+tolstoy");
+      const bookData2 = await response2.json();
+      const response3 = await fetch("http://openlibrary.org/search.json?author=dan+brown");
+      const bookData3 = await response3.json();
+      const response4 = await fetch("http://openlibrary.org/search.json?author=rowling");
+      const bookData4 = await response4.json();
+      setBooks([bookData, bookData2, bookData3, bookData4]);
     }
     getData();
   }, []);
