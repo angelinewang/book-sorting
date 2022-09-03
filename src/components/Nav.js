3b5c6324149cb4409cabe740406c53ff89764cb2
Mohamed <mohamed.sheik98@outlook.com>
2022-09-03 11:01:17 +0100

    fix nav and sort merge

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./Nav.css";
 
-function Nav({ data }) {
+function Nav({ data, author }) {
+  function handleClick(id) {
+    author(data[id]);
+  }
+
   return (
     <div className="navbar">
-      {data.map((author) => (
-        <button key={author.docs.key}>{author.docs[0].author_name[0]}</button>
+      {data.map((author, index) => (
+        <button
+          onClick={() => {
+            handleClick(index);
+          }}
+          key={author.docs.key}
+        >
+          {author.docs[0].author_name[0]}
+        </button>
       ))}
     </div>
   );
