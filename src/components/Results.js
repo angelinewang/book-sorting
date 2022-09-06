fe2bbc2e03e2c73331dcba7c2d3bb021b28ca545
Angeline Wang <02angelinewang@gmail.com>
2022-09-06 18:36:39 +0100

    Small formatting changes

--- src/components/Results.js
+++ src/components/Results.js
@@ -19,7 +19,7 @@ function Results ({checkedState, books}) {
             setSortedBooks([...sortedBooks].sort((a,b) => a.number_of_pages_median - b.number_of_pages_median));
         }
     
-        else if(checkedState.selectedOption === 2) {
+        else if (checkedState.selectedOption === 2) {
             setSortedBooks([...sortedBooks].sort(function(a, b) {
                 let textA = a.title.toUpperCase().trim().replace('"', "");
                 let textB = b.title.toUpperCase().trim().replace('"', "");
@@ -34,22 +34,16 @@ function Results ({checkedState, books}) {
     }, [books, checkedState]);
   
     console.log(`These are checked: ${checkedState}`)
-    return(
+    return (
         <>
         {sortedBooks.length > 0 ? (
-            <>
+        <>
             <Pagination sortedBooks={sortedBooks} dataLimit={5} pageLimit={5} />
-        
         </>
         ) : (<h1>No Books to display</h1>)
-    
     }
-           
         </>
     )
-
-
-
 }
 
 export default Results;
